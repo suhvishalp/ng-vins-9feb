3ed066a6116f44280761e955d57f0833ce198792
Vishal <suhvishal@gmail.com>
2020-02-09 03:08:24 -0800

    -

---
+++ myapp1/src/app/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  public companyName: string;
+  public logoImageUrl: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //initialize data members/properties of your class 
+    this.companyName = 'KHDA'
+    this.logoImageUrl = 'assets/logo.png'
+
+    setTimeout(() => {
+      this.companyName = 'Knowledge & Human Development Authority'
+    }, 6000);
+  }
+
+  handleShowClick($event){
+    console.log('Button is Clicked...')
+    console.log($event)
+  }
+
+  handleRightClick($event){
+    $event.preventDefault();
+    alert('Right click not allowed...')
+  }
+
+}
